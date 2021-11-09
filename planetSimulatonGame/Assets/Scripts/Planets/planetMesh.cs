26cbfbd4beea25a065d34301221523579e4f5df9
Kieron <kieronrana@gmail.com>
2021-11-09 22:25:00 +0000

    Size and resolution control for mesh.

--- planetSimulatonGame/Assets/Scripts/Planets/planetMesh.cs
+++ planetSimulatonGame/Assets/Scripts/Planets/planetMesh.cs
@@ -1,40 +1,73 @@
 using UnityEngine;
 
+struct CubeFaceVectors
+{
+    public Vector3 position;
+    public Vector3 rotate;
+
+    public CubeFaceVectors(Vector3 position, Vector3 rotate)
+    {
+        this.position = position;
+        this.rotate = rotate;
+    }
+}
+
 public class planetMesh : MonoBehaviour
 {
     //mesh
     private Mesh mesh;
-    public int width;
-    public int height;
+    public int xRes;
+    public int zRes;
+    public int size;
+    private int resolution;
     private Vector3[] vertices;
     private int[] triangles;
+    //face to cube vectors
+    public int diameter;
+    private CubeFaceVectors top = new CubeFaceVectors();
 
     private void Start()
+    {
+
+        CreateMesh(Vector3.zero, Vector3.zero);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            CreateMesh(Vector3.zero, Vector3.zero);
+        }
+    }
+
+    private void CreateMesh(Vector3 cubePosition, Vector3 cubeRotate)
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
-
-        CreateMesh();
-    }
-
-    private void CreateMesh()
-    {
-        vertices = new Vector3[(width + 1) * (height + 1)];
-        triangles = new int[width * height * 6];
+        vertices = new Vector3[(xRes + 1) * (zRes + 1)];
+        triangles = new int[xRes * zRes * 6];
+        resolution = xRes * zRes;
 
         SetVertices();
         CreateTriangles();
         UpdateMesh();
+        //FaceToCube(cubePosition, cubeRotate);
     }
 
     private void SetVertices()
     {
-        for (int index = 0, z = 0; z <= height; z++)
+        float xMultiplier = (float)size / xRes;
+        float zMultiplier = (float)size / zRes;
+        Debug.Log(xMultiplier);
+        Debug.Log(zMultiplier);
+
+        for (int index = 0, z = 0; z <= zRes; z++)
         {
-            for (int x = 0; x <= width; x++)
+            for (int x = 0; x <= xRes; x++)
             {
-                vertices[index] = new Vector3(x, 0, z);
+                Vector3 vertexPosition = new Vector3(x * xMultiplier, 0, z * zMultiplier);
+                vertices[index] = vertexPosition;
 
                 index++;
             }
@@ -43,16 +76,16 @@ public class planetMesh : MonoBehaviour
 
     private void CreateTriangles()
     {
-        for (int index = 0, triangleIndex = 0, z = 0; z < height; z++)
+        for (int index = 0, triangleIndex = 0, z = 0; z < zRes; z++)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < xRes; x++)
             {
                 triangles[triangleIndex] = index;
-                triangles[triangleIndex + 1] = index + width + 1;
+                triangles[triangleIndex + 1] = index + xRes + 1;
                 triangles[triangleIndex + 2] = index + 1;
                 triangles[triangleIndex + 3] = index + 1;
-                triangles[triangleIndex + 4] = index + width + 1;
-                triangles[triangleIndex + 5] = index + width + 2;
+                triangles[triangleIndex + 4] = index + xRes + 1;
+                triangles[triangleIndex + 5] = index + xRes + 2;
 
                 triangleIndex += 6;
                 index++;
@@ -68,5 +101,11 @@ public class planetMesh : MonoBehaviour
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
+    }
+
+    private void FaceToCube(Vector3 cubePosition, Vector3 cubeRotate)
+    {
+        transform.position = cubePosition;
+        transform.Rotate(cubeRotate);
     }
 }
